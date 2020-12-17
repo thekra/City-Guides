aadb22d1b2805a36e84828b9efc6e6d76b4fe7c6
Thekra Abuhaimed <th-f-a-a_1@hotmail.com>
2020-12-18 01:20:40 +0300

    Create the initial project

---
+++ City Guides/City Guides/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  City Guides
+//
+//  Created by Thekra Abuhaimed. on 03/05/1442 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
